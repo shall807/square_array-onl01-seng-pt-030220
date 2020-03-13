c6a617c7664dd5e95f33779bfa3b5104a9d4ffeb
Stevi Hall <career.sh.work@gmail.com>
2020-03-13 19:47:57 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
+#def square_array(array)
+  #array.each{|number| puts number**2}
+ # end
+
 def square_array(array)
-  # your code here
+  array.each{|numbers| puts number**2}
+  square_array.each
+
 end
+  
